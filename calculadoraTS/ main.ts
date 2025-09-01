2d37f317393d7562bdc842482f16ffde6bdc51db
Isadora <isadoraksilvino@gmail.com>
2025-09-01 13:07:35 -0300

    refactor: mudança de nomeação das variaveis

---
+++ calculadoraTS/ main.ts
@@ -0,0 +1,10 @@
+let numeroAtual: string = "";
+let operador: string | null = null;
+let primeiroNumero: number | null = null;
+
+// fazer funçoes de adicionarNumero(num: string), definirOperador(op: string) e limparDisplay() 
+//funçao calcular () com switch(operador)
+
+// if (numeroAnterior === null || operador === null || numeroAtual === "") return;
+//const segundoNumero = parseFloat(numeroAtual);
+    //let resultado: number;
